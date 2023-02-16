3aab68a279abd975b1c3271659e6f516552602ef
bernadettestella <bernadettestella27@gmail.com>
2023-02-16 03:45:32 -0800

    My first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+  * main - this is the main function
+  * Return: always zero
+  */
 int main(void)
 {
 	char str[100] = "Programming is like building a multilingual puzzle\n";
