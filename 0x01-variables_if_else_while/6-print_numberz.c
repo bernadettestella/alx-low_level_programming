2136e448516a11257be269a405d7ffe1e1676f8b
bernadettestella <bernadettestella27@gmail.com>
2023-02-17 17:22:55 -0800

    same problem different output

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - prits single digits
+  *
+  * Return: always zero if successful
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
