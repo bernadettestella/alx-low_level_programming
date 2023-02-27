241eb7c439f466b052474a8c43ca0d30b955a3d5
bernadettestella <bernadettestella27@gmail.com>
2023-02-27 11:52:55 -0800

    changes the value of the variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - sets the value of the integer to 98
+  *
+  * @n: the variable set
+  *
+  * Return: no return value since it is a void function
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
