381c7f840689849d97ce1733ce4138cd10ffa6bb
bernadettestella <bernadettestella27@gmail.com>
2023-04-04 18:19:26 -0700

    frees a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,17 +8,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *new;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
-
 	while (*head != NULL)
 	{
-		new = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		new = *head;
-
+		*head = temp;
 	}
 
 }
