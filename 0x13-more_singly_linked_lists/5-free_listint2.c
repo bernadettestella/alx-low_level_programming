1f2b68bf45293a9160ce961cd239f5992b92b5bf
bernadettestella <bernadettestella27@gmail.com>
2023-04-04 18:07:53 -0700

    frees a link

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a list
+  * @head: indicates a double pointer to the head of the link
+  *
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		*head = new->next;
+		free(*head);
+		new = *head;
+
+	}
+
+}
