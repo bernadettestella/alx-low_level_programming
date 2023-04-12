52ac621546ddb05a7ce95cab4ef6a7dba649ff2d
bernadettestella <bernadettestella27@gmail.com>
2023-04-11 19:33:27 -0700

    converts binary numbers to integers

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,26 +13,22 @@ unsigned int binary_to_uint(const char *b)
 	int j;
 	unsigned int f;
 
-	if (b == NULL)
+	f = 0;
+
+	if (!b)
 		return (0);
 
 	for (j = 0; b[j] != '\0'; j++)
 	{
 		if (b[j] != '0' && b[j] != '1')
-		{
 			return (0);
-		}
 	}
 
 	for (j = 0; b[j] != '\0'; j++)
 	{
-
 		f <<= 1;
-		
-		if (b[j] == 1)
-		
+		if (b[j] == '1')
 			f += 1;
-
 	}
 
 	return (f);
